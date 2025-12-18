7be3f6e069f5d489a8dd19155964634a8668f216
Kimdonghyuk0 <kdh367036@naver.com>
2025-12-19 05:27:55 +0900

    fix: pk err

--- ai-hkt/src/main/java/backend/aihkt/domain/book/entity/Book.java
+++ ai-hkt/src/main/java/backend/aihkt/domain/book/entity/Book.java
@@ -19,7 +19,7 @@ import lombok.NoArgsConstructor;
 public class Book {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
     @Column(name = "title", nullable= false)
